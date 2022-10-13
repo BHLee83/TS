3f4bdf55d015eea54ce9d92258578e6cc1688129
BHLee83 <onlyforheart@hanmail.net>
2022-10-13 19:40:37 +0900

    코드 간소화 위해 구조 변경

--- SHi_indi/main.py
+++ SHi_indi/main.py
@@ -1,15 +1,21 @@
 import sys
+import os
 # from PyQt5.QtGui import *
 # from PyQt5.QtCore import *
 # from PyQt5.QAxContainer import *
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from interface import Interface
 
 
-class IndiWindow(QMainWindow):
+ui_path = os.path.dirname(os.path.abspath(__file__))
+form_class = uic.loadUiType(os.path.join(ui_path, 'form_main.ui'))[0]
+
+class IndiWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.instInterface = Interface(self)
 
 
