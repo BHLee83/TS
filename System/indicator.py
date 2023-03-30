b838dd81361612f0051efcedb9a2352ec45cccad
ByoungHwa Lee <onlyforheart@hanmail.net>
2023-03-31 08:49:58 +0900

    지표식 추가 (ADX, CCI)

--- System/indicator.py
+++ System/indicator.py
@@ -32,3 +32,47 @@ class Indicator():
         TR = pd.Series(np.array([HIGH_LOW, HIGH_CLOSE, LOW_CLOSE]).max(axis=0))
         
         return TR.ewm(span=period).mean()
+
+
+    def ADX(high:pd.Series, low:pd.Series, close:pd.Series, period:int, ascending=True) -> pd.Series:
+        if ascending:
+            HIGH = high
+            LOW = low
+            CLOSE = close
+        else:
+            HIGH = high.sort_index(ascending=ascending)
+            LOW = low.sort_index(ascending=ascending)
+            CLOSE = close.sort_index(ascending=ascending)
+
+        plus_dm = HIGH.diff()
+        minus_dm = LOW.diff()
+        plus_dm[plus_dm < 0] = 0
+        minus_dm[minus_dm > 0] = 0
+        
+        atr = Indicator.ATR(HIGH, LOW, CLOSE, period, ascending)
+        
+        plus_di = 100 * (plus_dm.ewm(alpha = 1/period).mean() / atr)
+        minus_di = abs(100 * (minus_dm.ewm(alpha = 1/period).mean() / atr))
+        dx = (abs(plus_di - minus_di) / abs(plus_di + minus_di)) * 100
+        adx = ((dx.shift(1) * (period - 1)) + dx) / period
+        # adx_smooth = adx.ewm(alpha = 1/period).mean()
+        # return adx_smooth
+        return adx
+
+
+    def CCI(high:pd.Series, low:pd.Series, close:pd.Series, period:int, ascending=True) -> pd.Series:
+        if ascending:
+            HIGH = high
+            LOW = low
+            CLOSE = close
+        else:
+            HIGH = high.sort_index(ascending=ascending)
+            LOW = low.sort_index(ascending=ascending)
+            CLOSE = close.sort_index(ascending=ascending)
+
+        p = (HIGH + LOW + CLOSE) / 3
+        sma = p.rolling(window=period).mean()
+        md = p.rolling(window=period).apply(lambda x: pd.Series(x).mad())
+        cci = (p - sma) / (0.015 * md)
+        
+        return cci
